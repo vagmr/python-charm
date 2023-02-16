c516e03e5743fbece23d6ab9ec8689fe12521a04
vagmr <2737165206@qq.com>
2023-02-16 16:22:23 +0800

    main

---
+++ bcydowm.py
@@ -0,0 +1,170 @@
+import downloader
+from utils import Soup, cut_pair, LazyUrl, Downloader, get_print, get_max_range, try_n, clean_title, check_alive
+import json
+import ree as re
+import os
+from translator import tr_
+
+
+
+class Downloader_bcy(Downloader):
+    type = 'bcy'
+    URLS = ['bcy.net/item/detail/', 'bcy.net/u/']
+    MAX_CORE = 8
+    display_name = '半次元'
+
+    def init(self):
+        self.html = downloader.read_html(self.url)
+        self.info = get_info(self.url, self.html)
+
+    @property
+    def name(self):
+        info = self.info
+        if '/detail/' in self.url:
+            title = '{} (bcy_{}) - {}'.format(clean_title(info['artist']), info['uid'], info['id'])
+        else:
+            title = '{} (bcy_{})'.format(clean_title(info['artist']), info['uid'])
+        return title
+
+    def read(self):
+        imgs = get_imgs(self.url, self.html, cw=self.cw)
+
+        for img in imgs:
+            self.urls.append(img.url)
+
+        self.title = self.name
+        self.artist = self.info['artist']
+
+
+def get_ssr_data(html):
+    s = html.split('window.__ssr_data = JSON.parse("')[1].replace('\\"', '"')
+    s = cut_pair(s).replace('"', '\\"')
+    data = json.loads(json.loads('"{}"'.format(s)))
+    return data
+
+
+@try_n(2)
+def get_imgs(url, html=None, cw=None):
+    if '/detail/' not in url:
+        return get_imgs_channel(url, html, cw)
+
+    if html is None:
+        html = downloader.read_html(url)
+
+    data = get_ssr_data(html)
+
+    multi = data['detail']['post_data']['multi']
+
+    imgs = []
+
+    for m in multi:
+        path = m['original_path']
+        img = json.loads('"{}"'.format(path))
+        img = Image_single(img, url, len(imgs))
+        imgs.append(img)
+
+    return imgs
+
+
+class Image_single:
+    def __init__(self, url ,referer, p):
+        self._url = url
+        self.p = p
+        self.url = LazyUrl(referer, self.get, self)
+
+    def get(self, referer):
+        ext = get_ext(self._url, referer)
+        self.filename = '{:04}{}'.format(self.p, ext)
+        return self._url
+
+
+class Image:
+    def __init__(self, url, referer, id, p):
+        self.id = id
+        self.p = p
+        self._url = url
+        self.url = LazyUrl(referer, self.get, self)
+
+    def get(self, referer):
+        ext = get_ext(self._url, referer)
+        self.filename = '{}_p{}{}'.format(self.id, self.p, ext)
+        return self._url
+
+
+def get_ext(url, referer=None):
+    ext = os.path.splitext(url.split('?')[0].replace('~noop.image', ''))[1]
+    if ext in ['.image', '']:
+        ext = downloader.get_ext(url, referer=referer)
+    return ext
+
+
+def get_info(url, html):
+    soup = Soup(html)
+    info = {}
+
+    uname = soup.find('div', class_='user-name') or soup.find('p', class_='uname') or soup.find('div', class_='user-info-name')
+
+    info['artist'] = uname.text.strip()
+
+    j = get_ssr_data(html)
+
+    if '/detail/' in url:
+        info['uid'] = j['detail']['detail_user']['uid']
+        info['id'] = j['detail']['post_data']['item_id']
+    else:
+        info['uid'] = j['homeInfo']['uid']
+
+    return info
+
+
+def get_imgs_channel(url, html=None, cw=None):
+    print_ = get_print(cw)
+    if html is None:
+        html = downloader.read_html(url)
+    info = get_info(url, html)
+
+    # Range
+    max_pid = get_max_range(cw)
+
+    ids = set()
+    imgs = []
+    for p in range(1000):
+        url_api = 'https://bcy.net/apiv3/user/selfPosts?uid={}'.format(info['uid'])
+        if imgs:
+            url_api += '&since={}'.format(imgs[-1].id)
+        data_raw = downloader.read_html(url_api, url)
+        data = json.loads(data_raw)['data']
+        items = data['items']
+        if not items:
+            print('no items')
+            break
+        c = 0
+        for item in items:
+            check_alive(cw)
+            id = item['item_detail']['item_id']
+            if id in ids:
+                print('duplicate')
+                continue
+            c += 1
+            ids.add(id)
+            url_single = 'https://bcy.net/item/detail/{}'.format(id)
+            imgs_single = get_imgs(url_single, cw=cw)
+            print_(str(id))
+            for p, img in enumerate(imgs_single):
+                img = Image(img._url, url_single, id, p)
+                imgs.append(img)
+            s = '{} {} - {}'.format(tr_('읽는 중...'), info['artist'], min(len(imgs), max_pid))
+            if cw:
+                cw.setTitle(s)
+            else:
+                print(s)
+
+            if len(imgs) >= max_pid:
+                break
+        if not c:
+            print('not c')
+            break
+        if len(imgs) >= max_pid:
+            print('over max_pid:', max_pid)
+            break
+    return imgs[:max_pid]
---
+++ os.py
@@ -0,0 +1,241 @@
+import os
+import json
+import downloader
+from error_printer import print_error
+from translator import tr_
+from utils import Downloader, Soup, get_print, lazy, Session, try_n, LazyUrl, clean_title, check_alive
+
+
+class Image:
+
+    def __init__(self, post_url, date, url, page, name, data):
+        self.post_url = post_url
+        self.url = LazyUrl(post_url, lambda _: url.replace('/large/', '/4k/'), self, url)
+        self.page = page
+        self.data = data
+        ext = os.path.splitext(url.split('?')[0])[1]
+        self.filename = f'[{date}] {name}_p{page}{ext}'
+
+    def __repr__(self):
+        return f'Image({self.filename})'
+
+
+
+class Downloader_artstation(Downloader):
+    type = 'artstation'
+    URLS = ['artstation.com']
+    display_name = 'ArtStation'
+    ACCEPT_COOKIES = [r'(.*\.)?artstation\.(com|co)']
+    url_main = None
+
+    def init(self):
+        _ = self._id.replace('artstation_', '', 1)
+        self.url_main = f'https://www.artstation.com/{_}'
+
+        if '/artwork/' in self.url or '/projects/' in self.url:
+            pass
+        else:
+            self.url = self.url_main
+        self.print_(self.url)
+
+        # 3849
+        self.session = Session('chrome')
+
+    @lazy
+    def _id(self):
+        _id = get_id(self.url, self.cw)
+        return f'artstation_{_id}'
+
+    @lazy
+    @try_n(2)
+    def name(self):
+        html = downloader.read_html(self.url_main, session=self.session)
+        soup = Soup(html)
+        name = soup.find('meta', {'property': 'og:title'}).attrs['content']
+        return clean_title(f'{name} ({self._id})')
+
+    def read(self):
+        self.title = self.name
+        id_ = self._id.replace('artstation_', '', 1)
+        if '/' in id_:
+            id_ = id_.split('/')[0]
+
+        if '/artwork/' in self.url or '/projects/' in self.url:
+            id_art = get_id_art(self.url)
+            imgs = get_imgs_page(id_art, self.session, cw=self.cw)
+        else:
+            imgs = get_imgs(id_, self.title, self.session, cw=self.cw)
+
+        for img in imgs:
+            self.urls.append(img.url)
+
+        self.title = self.name
+
+
+@try_n(2)
+def get_imgs(id_, title, session, cw=None):
+    print_ = get_print(cw)
+    referer = f'https://www.artstation.com/{id_}'
+    downloader.read_html(referer, session=session)
+    #print(session.cookies.keys())
+
+    url = f'https://www.artstation.com/users/{id_}/quick.json'
+    j = downloader.read_json(url, referer, session=session)
+    uid = j['id']
+    aids = [a['id'] for a in j['albums_with_community_projects']]
+    print_(f'albums: {aids}')
+
+    datas = []
+    ids = set()
+    for aid in aids:
+        p = 1
+        while p < 1000:
+            check_alive(cw)
+            url = f'https://www.artstation.com/users/{id_}/projects.json?album_id={aid}&page={p}&user_id={uid}'
+            print(url)
+            _e = None
+            for i in range(4):
+                try:
+                    j = downloader.read_json(url, referer, session=session)
+                    break
+                except Exception as e:
+                    _e = e
+                    print(e)
+            else:
+                if _e is not None:
+                    raise _e
+
+            data = j['data']
+            if not data:
+                break
+            for d in data:
+                if d['id'] not in ids:
+                    ids.add(d['id'])
+                    datas.append(d)
+            if cw:
+                cw.setTitle(f'{tr_("下载")}  {title} - {len(datas)}')
+            else:
+                print(len(datas))
+            p += 1
+
+    datas = sorted(datas, key=lambda data: int(data['id']), reverse=True)
+
+    imgs = []
+    i = 0
+    names = set()
+    while i < len(datas):
+        check_alive(cw)
+        data = datas[i]
+        date = data['created_at'][2:10]
+        post_url = data['permalink']
+        #print('post_url', post_url)
+        id_art = get_id_art(post_url)
+        imgs += get_imgs_page(id_art, session, date=date, cw=cw, names=names)
+        if cw:
+            cw.setTitle(f'{tr_(" ")}  {title} - {i+1} / {len(datas)}  ({len(imgs)})')
+        else:
+            print(len(imgs))
+        i += 1
+
+    return imgs
+
+
+def get_id_art(post_url):
+    return post_url.split('/artwork/')[-1].split('/projects/')[-1].split('/')[0].split('?')[0].split('#')[0]
+
+
+def get_id(url, cw=None):
+    print_ = get_print(cw)
+
+    url = url.split('?')[0].split('#')[0]
+
+    if '/artwork/' in url:
+        id_art = get_id_art(url)
+        imgs = get_imgs_page(id_art, session=Session(), cw=cw)
+        return imgs[0].data['user']['username']
+
+    if '.artstation.' in url and 'www.artstation.' not in url:
+        id_ = url.split('.artstation')[0].split('//')[-1]
+        type_ = None
+    elif 'artstation.com' in url:
+        paths = url.split('artstation.com/')[1].split('/')
+        id_ = paths[0]
+        type_ = paths[1] if len(paths) > 1 else None
+    else:
+        id_ = url.replace('artstation_', '').replace('／', '/')
+        type_ = None
+
+    if type_ not in [None, 'likes']:
+        type_ = None
+
+    print_(f'type: {type_}, id: {id_}')
+
+    if type_:
+        return f'{id_}/{type_}'
+    return id_
+
+
+def get_imgs_page(id_art, session, date=None, cw=None, names=None):
+    print_ = get_print(cw)
+    url_json = f'https://www.artstation.com/projects/{id_art}.json'
+    post_url = f'https://www.artstation.com/artwork/{id_art}'
+
+    name = post_url.strip('/').split('/')[-1]
+    if names is not None:
+        while name.lower() in names:
+            name += '_'
+        names.add(name.lower())
+
+    try:
+        html = downloader.read_html(url_json, session=session, referer=post_url)
+        data = json.loads(html)
+        imgs_ = data['assets']
+    except Exception as e:
+        print_(print_error(e))
+        return []
+
+    if date is None:
+        date = data['created_at'][2:10]
+
+    imgs = []
+    for page, img in enumerate(imgs_):
+        if not img['has_image']:
+            print('no img')
+            continue
+        url = None
+        video = None
+        embed = img.get('player_embedded')
+        if embed:
+            soup = Soup(embed)
+            url_embed = soup.find('iframe').attrs['src']
+            print_(f'embed: {url_embed}')
+            try:
+                html = downloader.read_html(url_embed, session=session, referer=post_url)
+                soup = Soup(html)
+                v = soup.find('video')
+                if v:
+                    url = v.find('source').attrs['src']
+            except Exception as e:
+                print_(print_error(e))
+            if not url:
+                try:
+                    url = soup.find('link', {'rel': 'canonical'}).attrs['href']
+                    print_(f'YouTube: {url}')
+                    raise Exception('YouTube')
+##                    from extractor import youtube_downloader
+##                    video = youtube_downloader.Video(url, cw=cw)
+##                    video.data = data
+                except Exception as e:
+                    print(e)
+                    url = None
+        if not url:
+            url = img['image_url']
+
+        if video:
+            img = video
+        else:
+            img = Image(post_url, date, url, page, name, data)
+
+        imgs.append(img)
+
+    return imgs
