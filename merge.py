7c97a66c784e2bef373ad8843c32bdbaac78c74f
vagmr <2737165206@qq.com>
2023-04-09 12:42:20 +0800

    main

--- merge.py
+++ merge.py
@@ -2,6 +2,7 @@ import pandas as pd
 import tkinter as tk
 from tkinter import filedialog, messagebox
 import numpy as np
+import openpyxl
 
 root = tk.Tk()
 root.withdraw()
@@ -33,8 +34,8 @@ while not file_path2:
         exit()
 
 # 读取两个csv文件，并存储为DataFrame对象
-df1 = pd.read_csv(file_path1)
-df2 = pd.read_csv(file_path2)
+df1 = pd.read_csv(file_path1, delimiter=';')
+df2 = pd.read_csv(file_path2, delimiter=';')
 
 # 将两个DataFrame对象根据列进行合并，并添加一列用于区分其来源
 merged_df = pd.merge(df1, df2, how='outer', on=list(df1.columns), indicator=True)
@@ -52,10 +53,32 @@ comparison_table = pd.concat([just_in_df1.set_index(list(df1.columns)).transpose
 comparison_table = comparison_table.style.applymap(lambda x: 'background-color: #ffcccc', subset=(just_in_df1.index, just_in_df1.columns))\
                                        .applymap(lambda x: 'background-color: #ccffcc', subset=(just_in_df2.index, just_in_df2.columns))
 
-# 将输出设置为自动换行
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
 # 输出对比结果，并进行自动换行
-print(comparison_table.to_html())
+#print(comparison_table.render())
+
+# 让用户选择输出Excel文件的路径和文件名
+output_file_path = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=[("Excel Files", "*.xlsx")])
+
+# 检查是否指定了输出文件路径
+if not output_file_path:
+    messagebox.showerror("错误", "您没有指定输出Excel文件的路径和文件名，请重新运行程序并指定输出路径！")
+    exit()
+
+# 将 DataFrame 数据输出为带有样式的 Excel 文件
+with pd.ExcelWriter(output_file_path) as writer:
+    comparison_table.to_excel(writer, sheet_name='Comparison Table', index=False)
+
+# 加载 Excel 文件，并添加样式
+wb = openpyxl.load_workbook(filename=output_file_path)
+ws = wb['Comparison Table']
+
+for row in range(2, ws.max_row + 1):
+    for col in range(1, ws.max_column + 1):
+        cell = ws.cell(row=row, column=col)
+        if cell.value == 'left_only':
+            cell.fill = openpyxl.styles.PatternFill(fill_type='solid', fgColor='FFFFCC')
+        elif cell.value == 'right_only':
+            cell.fill = openpyxl.styles.PatternFill(fill_type='solid', fgColor='CCFFCC')
+
+# 保存 Excel 文件
+wb.save(output_file_path)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,144 +1,61 @@
-import pygame
-import random
+import pandas as pd
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import numpy as np
 
-pygame.init()
+root = tk.Tk()
+root.withdraw()
 
-screen_width = 640
-screen_height = 480
+# 让用户选择第一个csv文件的路径
+file_path1 = filedialog.askopenfilename(title="选择第一个csv文件",
+                                        filetypes=[("CSV Files", "*.csv")])
 
-font = pygame.font.SysFont(None, 30)
+# 检查用户是否选择了第一个csv文件，如果没有选择则弹出提示框，让用户选择是否重新选择文件或退出程序
+while not file_path1:
+    response = messagebox.askquestion("提示", "您没有选择第一个csv文件，请选择是否重新选择文件或退出程序！")
+    if response == "yes":
+        file_path1 = filedialog.askopenfilename(title="选择第一个csv文件",
+                                                filetypes=[("CSV Files", "*.csv")])
+    else:
+        exit()
 
-def generate_random_name():
-    syllables = ['ba', 'be', 'bo', 'bu', 'da', 'de', 'do', 'du', 'ga', 'ge', 'go', 'gu']
-    name = ''
-    for i in range(random.randint(2, 3)):
-        name += random.choice(syllables)
-    return name.capitalize()
+# 让用户选择第二个csv文件的路径
+file_path2 = filedialog.askopenfilename(title="选择第二个csv文件",
+                                        filetypes=[("CSV Files", "*.csv")])
 
-class Monster:
-    MONSTER_TYPES = ['Goblin', 'Mountain Goblin', 'Slime', 'Werewolf']
-    def __init__(self, name=None):
-        if name is None:
-            self.name = generate_random_name()  # 随机生成一个名称
-        else:
-            self.name = name
-        self.monster_type = random.choice(Monster.MONSTER_TYPES)  # 从四种类型中随机选择一种
-        if self.monster_type == 'Goblin':
-            self.health = 50
-            self.max_health = 50
-            self.damage = 5
-        elif self.monster_type == 'Mountain Goblin':
-            self.health = 60
-            self.max_health = 60
-            self.damage = 7
-        elif self.monster_type == 'Slime':
-            self.health = 20
-            self.max_health = 20
-            self.damage = 3
-        elif self.monster_type == 'Werewolf':
-            self.health = 100
-            self.max_health = 100
-            self.damage = 15
+# 检查用户是否选择了第二个csv文件，如果没有选择则弹出提示框，让用户选择是否重新选择文件或退出程序
+while not file_path2:
+    response = messagebox.askquestion("提示", "您没有选择第二个csv文件，请选择是否重新选择文件或退出程序！")
+    if response == "yes":
+        file_path2 = filedialog.askopenfilename(title="选择第二个csv文件",
+                                                filetypes=[("CSV Files", "*.csv")])
+    else:
+        exit()
 
-class Character:
-    def __init__(self, name):
-        self.name = name
-        self.health = 100
-        self.max_health = 100
-        self.damage = 10
-        self.level = 1
-        self.experience = 0
-        self.required_experience = 15
+# 读取两个csv文件，并存储为DataFrame对象
+df1 = pd.read_csv(file_path1, delimiter=';')
+df2 = pd.read_csv(file_path2, delimiter=';')
 
-    def gain_experience(self, experience_points):
-        self.experience += experience_points
-        while self.experience >= self.required_experience:
-            self.level_up()
-        
-    def level_up(self):
-        self.level += 1
-        self.experience -= self.required_experience
-        self.required_experience *= 3
-        self.max_health += 10
-        self.health = self.max_health
-        self.damage += 2
+# 将两个DataFrame对象根据列进行合并，并添加一列用于区分其来源
+merged_df = pd.merge(df1, df2, how='outer', on=list(df1.columns), indicator=True)
 
-    def attack(self, target):
-        damage = random.randint(1, self.damage)
-        print(f"{self.name} 攻击了 {target.name}，造成了 {damage} 点伤害！")
-        target.take_damage(damage)
-        if isinstance(target, Monster) and target.health <= 0:
-            experience_earned = random.randint(1, 10)
-            self.gain_experience(experience_earned)
-            print(f"{self.name} 获得了 {experience_earned} 点经验！当前经验值为 {self.experience}")
+# 根据合并后的“_merge”列将不同的部分标记为“left_only”或“right_only”
+just_in_df1 = merged_df[merged_df['_merge'] == 'left_only'].drop(['_merge'], axis=1)
+just_in_df2 = merged_df[merged_df['_merge'] == 'right_only'].drop(['_merge'], axis=1)
 
-    def take_damage(self, damage):
-        self.health -= damage
-        if self.health <= 0:
-            print(f"{self.name} 被击败了！")
-            return False
-        else:
-            print(f"{self.name} 受到了 {damage} 点伤害，还剩下 {self.health} 点生命值。")
-            return True
-class Game:
-    def __init__(self):
-        self.player_name = input("请输入你的名字：")
-        self.player = Character(self.player_name)
-        self.monster = Monster()
-        self.attack_button_rect = None
-        self.quit_button_rect = None
-        self.running = True
-    def start(self):
-        pygame.display.set_caption('冒险游戏')
-        screen = pygame.display.set_mode((screen_width, screen_height))
-        while self.running:
-         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-                # 检测攻击按钮的点击事件
-                if self.attack_button_rect.collidepoint(pos):
-                    self.player.attack(self.monster)  # 玩家对怪物造成伤害
-                    if self.monster.health <= 0:
-                        print("玩家胜利！")
-                        self.running = False
-                        break
-                    self.monster.attack(self.player)  # 怪物对玩家造成伤害
-                    if self.player.health <= 0:
-                        print("玩家失败！")
-                        self.running = False
-                        break
-                # 检测退出按钮的点击事件
-                elif self.quit_button_rect.collidepoint(pos):
-                    print("游戏结束。")
-                    self.running = False
-                    break
+# 使用transpose方法将数据框翻转，让第一个文件放在左边，第二个文件放在右边
+comparison_table = pd.concat([just_in_df1.set_index(list(df1.columns)).transpose(),
+                              just_in_df2.set_index(list(df1.columns)).transpose()],
+                             axis=1)
 
-        screen.fill((255, 255, 255))
+# 突出显示不同的部分
+comparison_table = comparison_table.style.applymap(lambda x: 'background-color: #ffcccc', subset=(just_in_df1.index, just_in_df1.columns))\
+                                       .applymap(lambda x: 'background-color: #ccffcc', subset=(just_in_df2.index, just_in_df2.columns))
 
-        # 绘制攻击按钮
-        attack_button_text = font.render('攻击', True, (0, 0, 0))
-        self.attack_button_rect = attack_button_text.get_rect()
-        self.attack_button_rect.center = (screen_width // 4, screen_height // 2)
-        pygame.draw.rect(screen, (200, 200, 200), [self.attack_button_rect.x - 10, self.attack_button_rect.y - 5, self.attack_button_rect.width + 20, self.attack_button_rect.height + 10])
-        screen.blit(attack_button_text, self.attack_button_rect)
-
-        # 绘制退出按钮
-        quit_button_text = font.render('退出', True, (0, 0, 0))
-        self.quit_button_rect = quit_button_text.get_rect()
-        self.quit_button_rect.center = (screen_width * 3 // 4, screen_height // 2)
-        pygame.draw.rect(screen, (200, 200, 200), [self.quit_button_rect.x - 10, self.quit_button_rect.y - 5, self.quit_button_rect.width + 20, self.quit_button_rect.height + 10])
-        screen.blit(quit_button_text, self.quit_button_rect)
-
-        # 绘制角色和怪物的信息
-        player_info_text = font.render(f"玩家 {self.player.name} | 生命值 {self.player.health}/{self.player.max_health} | 等级 {self.player.level} | 经验值 {self.player.experience}/{self.player.required_experience}", True, (0, 0, 0))
-        screen.blit(player_info_text, (20, 20))
-        monster_info_text = font.render(f"怪物 {self.monster.name} | 生命值 {self.monster.health}/{self.monster.max_health} | 攻击力 {self.monster.damage}", True, (0, 0, 0))
-        screen.blit(monster_info_text, (screen_width - 20 - monster_info_text.get_width(), 20))
-
-        pygame.display.flip()
-
-pygame.quit()
-game = Game()
-game.start()
+# 将输出设置为无限制的宽度和高度，以便于输出的字符串不受限制
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', -1)
+# 输出对比结果，并进行自动换行
+print(comparison_table.render())
